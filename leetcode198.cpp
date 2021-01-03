ab5127ae15d1ca83f3da7b89155b3c09f5f6b65e
zhouyuming <1006129869@qq.com>
2021-01-03 14:02:40 +0800

    update 198

--- leetcode198.cpp
+++ leetcode198.cpp
@@ -1,3 +1,14 @@
 int rob(int* nums, int numsSize){
+    if (!numsSize) {
+        return 0;
+    }
 
+    int dp[numsSize][2];
+    dp[0][0] = 0;
+    dp[0][1] = nums[0];
+    for (int i = 1; i < numsSize; i++) {
+        dp[i][0] = dp[i - 1][0] > dp[i - 1][1] ? dp[i - 1][0] : dp[i - 1][1];
+        dp[i][1] = nums[i] + dp[i - 1][0];
+    }
+    return dp[numsSize - 1][0] > dp[numsSize - 1][1] ? dp[numsSize - 1][0] : dp[numsSize - 1][1];
 }
