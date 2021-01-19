8b5059637eb52ddc34d8954884a0b610c0a2c839
zhouyuming <1006129869@qq.com>
2021-01-19 23:31:41 +0800

    update 209

--- 201-300/leetcode209.cpp
+++ 201-300/leetcode209.cpp
@@ -2,37 +2,37 @@
 #include <math.h>
 #include <climits>
 
-/*int minSubArrayLen(int s, int* nums, int numsSize){
-    if (numsSize == 0) {
-        return 0;
+int lower_bound(int *a, int l, int r, int q)
+{
+    if (a[r] < q) {
+        return -1;
     }
-    int ans = INT_MAX;
-    for (int i = 0; i < numsSize; i++) {
-        int sum = 0;
-        for (int j = i; j < numsSize; j++) {
-            sum += nums[j];
-            if (sum >= s) {
-                ans = fmin(ans, j - i + 1);
-                break;
-            }
+    while (l < r) {
+        int mid = (l + r) >> 1;
+        if (a[mid] >= q) {
+            r = mid;
+        } else {
+            l = mid + 1;
         }
     }
-    return ans == INT_MAX ? 0 : ans;
-}*/
+    return l;
+}
 
 int minSubArrayLen(int s, int* nums, int numsSize){
     if (numsSize == 0) {
         return 0;
     }
     int ans = INT_MAX;
-    for (int i = 0; i < numsSize; i++) {
-        int sum = 0;
-        for (int j = i; j < numsSize; j++) {
-            sum += nums[j];
-            if (sum >= s) {
-                ans = fmin(ans, j - i + 1);
-                break;
-            }
+    int *sums = (int*)malloc(sizeof(int) * (numsSize + 1));
+    memset(sums, 0, sizeof(int) * (numsSize + 1));
+    for (int i = 1; i <= numsSize; i++) {
+        sums[i] = sums[i - 1] + nums[i - 1];
+    }
+    for (int i = 1; i <= numsSize; i++) {
+        int target = s + sums[i - 1];
+        int bound = lower_bound(sums, 1, numsSize, target);
+        if (bound != -1) {
+            ans = fmin(ans, bound - (i - 1));
         }
     }
     return ans == INT_MAX ? 0 : ans;
