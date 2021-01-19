8648406cf473fb5c0aa242eb7b102b79a760a8e9
zhouyuming <1006129869@qq.com>
2021-01-19 23:41:02 +0800

    update 209

--- 201-300/leetcode209.cpp
+++ 201-300/leetcode209.cpp
@@ -42,7 +42,19 @@ int minSubArrayLen(int s, int* nums, int numsSize){
     if (numsSize == 0) {
         return 0;
     }
-    
+    int ans = INT_MAX;
+    int start = 0;
+    int end = 0;
+    int sum = 0;
+    while (end < numsSize) {
+        sum += nums[end];
+        while (sum >= s) {
+            ans = fmin(ans, end - start + 1);
+            sum -= nums[start];
+            start++;
+        }
+        end++;
+    }
     return ans == INT_MAX ? 0 : ans;
 }
 
