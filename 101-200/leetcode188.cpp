63226f1aedde986b0ccae3b48e66e84d4a676800
zhouyuming <1006129869@qq.com>
2021-02-06 23:28:09 +0800

    update 188

--- 101-200/leetcode188.cpp
+++ 101-200/leetcode188.cpp
@@ -1,20 +1,41 @@
-#define MIN(a,b) ((a)<(b) ? (a) : (b))
-#define MAX(a,b) ((a)<(b) ? (b) : (a))
+#include <gtest/gtest.h>
+#include <math.h>
 
 int maxProfit(int k, int* prices, int pricesSize){
-    if (pricesSize <= 1 || k < 1) {
+    int n = pricesSize;
+    if (n == 0) {
         return 0;
     }
-    int *k_buy = (int*)malloc(k * sizeof(int));
-    int *k_sold = (int*)calloc(k * sizeof(int));
-    for (int i = 0; i < k; i++) {
-        k_buy[i] = 0 - prices[0];
+    k = fmin(k, n / 2);
+    int buy[n][k + 1], sell[n][k + 1];
+    memset(buy, 0, sizeof(buy));
+    memset(sell, 0, sizeof(sell));
+    buy[0][0] = -prices[0];
+    sell[0][0] = 0;
+    for (int i = 1; i <= k; ++i) {
+        buy[0][i] = sell[0][i] = INT_MIN / 2;
     }
-
-    for (int i = 1; i < pricesSize; i++) {
-        for (int j = k - 1; j >= 1; j--) {
-            k_sold[j] = MAX(k_sold[j], k_buy[j] + prices[i]);
-            k_buy[j] = MAX(k_buy[j], k_sold[j-1] - prices[i]);
+    for (int i = 1; i < n; ++i) {
+        buy[i][0] = fmax(buy[i - 1][0], sell[i - 1][0] - prices[i]);
+        for (int j = 1; j <= k; ++j) {
+            buy[i][j] = fmax(buy[i - 1][j], sell[i - 1][j] - prices[i]);
+            sell[i][j] = fmax(sell[i - 1][j], buy[i - 1][j - 1] + prices[i]);
         }
     }
+     int ret = 0;
+    for (int i = 0; i <= k; i++) {
+        ret = fmax(ret, sell[n - 1][i]);
+    }
+     return ret;
+}
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
+TEST(LeetCode, num188x001)
+{
+
 }
