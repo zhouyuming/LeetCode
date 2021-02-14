30c5fa99c33d68a4fac3e7a784447e9f2dda4d32
zhouyuming <1006129869@qq.com>
2021-02-14 21:39:49 +0800

    update 225

---
+++ 201-300/leetcode225.cpp
@@ -0,0 +1,46 @@
+#include <gtest/gtest.h>
+
+typedef struct {
+
+} MyStack;
+
+/** Initialize your data structure here. */
+
+MyStack* myStackCreate() {
+
+}
+
+/** Push element x onto stack. */
+void myStackPush(MyStack* obj, int x) {
+
+}
+
+/** Removes the element on top of the stack and returns that element. */
+int myStackPop(MyStack* obj) {
+
+}
+
+/** Get the top element. */
+int myStackTop(MyStack* obj) {
+
+}
+
+/** Returns whether the stack is empty. */
+bool myStackEmpty(MyStack* obj) {
+
+}
+
+void myStackFree(MyStack* obj) {
+
+}
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
+TEST(LeetCode, num225x001)
+{
+
+}
