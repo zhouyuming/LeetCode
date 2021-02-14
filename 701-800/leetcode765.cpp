e3b6953527902b34af925901f6f08ce598e07a0b
zhouyuming <1006129869@qq.com>
2021-02-14 13:36:31 +0800

    update 765

---
+++ 701-800/leetcode765.cpp
@@ -0,0 +1,69 @@
+#include <gtest/gtest.h>
+
+int getf(int* f, int x)
+{
+    if (f[x] == x) {
+        return x;
+    }
+    int newf = getf(f, f[x]);
+    f[x] = newf;
+    return newf;
+}
+
+void add(int *f, int x, int y)
+{
+    int fx = getf(f, x);
+    int fy = getf(f, y);
+    f[fx] = fy;
+}
+
+int minSwapsCouples(int* row, int rowSize){
+    int n = rowSize;
+    int tot = n / 2;
+    int f[tot];
+
+    memset(f, 0, sizeof(f));
+    for (int i = 1; i < tot; i++) {
+        f[i] = i;
+    }
+
+    for (int i = 0; i < n; i += 2) {
+        int l = row[i] / 2;
+        int r = row[i + 1] / 2;
+        add(f, l, r);
+    }
+
+    int m[tot];
+    memset(m, 0, sizeof(m));
+    for (int i = 0; i < tot; i++) {
+        int fx = getf(f, i);
+        m[fx]++;
+    }
+    int ret = 0;
+    for (int i = 0; i < tot; i++) {
+        if (f[i] == i) {
+            ret += m[i] - 1;
+        }
+    }
+    return ret;
+}
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
+TEST(LeetCode, num765x001)
+{
+    int nums[4] = {0, 2, 1, 3};
+    int res = minSwapsCouples(nums, 4);
+    EXPECT_EQ(res, 1);
+}
+
+TEST(LeetCode, num765x002)
+{
+    int nums[4] = {3, 2, 0, 1};
+    int res = minSwapsCouples(nums, 4);
+    EXPECT_EQ(res, 0);
+}
--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-g++ -std=c++11 401-500/leetcode424.cpp -o test -lgtest -lpthread
+g++ -std=c++11 701-800/leetcode765.cpp -o test -lgtest -lpthread
