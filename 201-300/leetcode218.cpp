762e4bee116d5f67dfaa2662ab307edd1b41f112
zhouyuming <1006129869@qq.com>
2021-02-14 11:45:19 +0800

    update 765

---
+++ 201-300/leetcode218.cpp
@@ -0,0 +1,16 @@
+#include <gtest/gtest.h>
+
+int** getSkyline(int** buildings, int buildingsSize, int* buildingsColSize, int* returnSize, int** returnColumnSizes){
+
+}
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
+TEST(LeetCode, num218x001)
+{
+    
+}
---
+++ 601-700/leetcode665.cpp
@@ -0,0 +1,21 @@
+#include <gtest/gtest.h>
+
+bool checkPossibility(int* nums, int numsSize){
+    int num = 0;
+    for (int i = 0; i < numsSize - 1; i++) {
+        if (nums[i] > nums[i + 1]) {
+            
+        }
+    }
+}
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
+TEST(LeetCode, num665x001)
+{
+    
+}
