412fb89963f090b98c4e7f6c2e585948aeb16262
zhouyuming <1006129869@qq.com>
2021-02-15 14:46:43 +0800

    update 226

---
+++ 201-300/leetcode226.cpp
@@ -0,0 +1,31 @@
+#include <gtest/gtest.h>
+
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+};
+
+struct TreeNode* invertTree(struct TreeNode* root){
+    struct TreeNode* tmp = NULL;
+
+    if (root != NULL) {
+        tmp = root->left;
+        root->left = root->right;
+        root->right = tmp;
+        invertTree(root->left);
+        invertTree(root->right);
+    }
+    return root;
+}
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
+TEST(LeetCode, num201x001)
+{
+
+}
