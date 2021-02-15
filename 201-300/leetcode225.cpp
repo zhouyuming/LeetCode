110bd0bede4578526d5a79fae3efb294bba6be6a
zhouyuming <1006129869@qq.com>
2021-02-15 11:56:46 +0800

    update 225

--- 201-300/leetcode225.cpp
+++ 201-300/leetcode225.cpp
@@ -1,13 +1,57 @@
 #include <gtest/gtest.h>
 
-typedef struct {
+#define LEN 20
 
+typedef struct queue {
+    int *data;
+    int head;
+    int rear;
+    int size;
+} Queue;
+
+typedef struct {
+    Queue *queue1, *queue2;s
 } MyStack;
+
+Queue *initQueue(int k) {
+    Queue *obj = (Queue *)malloc(sizeof(Queue));
+    obj->data = (int *)malloc(k * sizeof(int));
+    obj->head = -1;
+    obj->rear = -1;
+    obj->size = k;
+    return obj;
+}
+
+void enQueue(Queue *obj, int e) {
+    if (obj->head == -1) {
+        obj->head = 0;
+    }
+    obj->rear = (obj->rear + 1) % obj->size;
+    obj->data[obj->rear] = e;
+}
+
+int deQueue(Queue *obj) {
+    int a = obj->data[obj->head];
+    if (obj->head == obj->rear) {
+        obj->rear = -1;
+        obj->head = -1;
+        return a;
+    }
+    obj->head = (obj->head + 1) % obj->size;
+    return a;
+}
+
+int isEmpty(Queue *obj) {
+    return obj->head == -1;
+}
 
 /** Initialize your data structure here. */
 
 MyStack* myStackCreate() {
-
+    MyStack *obj = (MyStack *)malloc(sizeof(MyStack));
+    obj->queue1 = initQueue(LEN);
+    obj->queue2 = initQueue(LEN);
+    return obj;
 }
 
 /** Push element x onto stack. */
