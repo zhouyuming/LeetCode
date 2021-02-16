7f2fbed3d747168ecaa709f43207635af4d68574
zhouyuming <1006129869@qq.com>
2021-02-16 21:45:23 +0800

    update 236

---
+++ 201-300/leetcode236.cpp
@@ -0,0 +1,32 @@
+#include <gtest/gtest.h>
+
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+};
+
+struct TreeNode* lowestCommonAncestor(struct TreeNode* root, struct TreeNode* p, struct TreeNode* q) {
+    if (root == NULL || root == p || root == q) {
+        return root;
+    }
+
+    struct TreeNode *left = lowestCommonAncestor(root->left, p, q);
+    struct TreeNode *right = lowestCommonAncestor(root->right, p, q);
+
+    if (left != NULL && right != NULL) {
+        return root;
+    }
+    return left == NULL ? right : left;
+}
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
+TEST(LeetCode, num236x001)
+{
+
+}
