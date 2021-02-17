4121b3dd47e95e079fc7f06430e65b3e6002a367
zhouyuming <1006129869@qq.com>
2021-02-17 15:56:10 +0800

    update 237

---
+++ 201-300/leetcode237.cpp
@@ -0,0 +1,22 @@
+#include <gtest/gtest.h>
+
+struct ListNode {
+    int val;
+    struct ListNode *next;
+};
+
+void deleteNode(struct ListNode* node) {
+    
+}
+
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
+TEST(LeetCode, num237x001)
+{
+
+}
