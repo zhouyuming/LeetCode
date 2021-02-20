470bbe94422072ba89d41c8fac2da98c600b89bc
zhouyuming <1006129869@qq.com>
2021-02-20 20:18:26 +0800

    update 239

--- 201-300/leetcode239.cpp
+++ 201-300/leetcode239.cpp
@@ -1,7 +1,39 @@
 #include <gtest/gtest.h>
 
-int* maxSlidingWindow(int* nums, int numsSize, int k, int* returnSize){
+struct Heap {
+    int **heap;
+    int size;
+    int capacity;
+};
 
+void init(struct Heap* obj, int capacity)
+{
+    obj->size = 0;
+    obj->capacity = capacity;
+    obj->heap = (int**)malloc(sizeof(int*) * (obj->capacity + 1));
+    for (int i = 1; i <= obj->capacity; i++) {
+        obj->heap[i] = (int*)malloc(sizeof(int) * 2);
+    }
+}
+
+void push(struct Heap* obj, int num0, int num1)
+{
+    int sub1 = ++(obj->size);
+    int sub2 = sub1 >> 1;
+
+    (obj->heap[sub1])[0] = num0;
+    (obj->heap[sub1])[0] = num1;
+    while (sub2 > 0 && cmp() < 0) {
+        
+    } 
+}
+
+int* maxSlidingWindow(int* nums, int numsSize, int k, int* returnSize){
+    struct Heap* q = (struct Heap*)malloc(sizeof(struct Heap));
+    init(q, numsSize);
+    for (int i = 0; i < k; i++) {
+        push(q, nums[i], i);
+    }
 }
 
 int main(int argc, char **argv)
