db0a4b91d65709855957fed21a6e3947c448b2b5
zhouyuming <1006129869@qq.com>
2021-02-21 20:45:42 +0800

    update 241

--- 201-300/leetcode241.cpp
+++ 201-300/leetcode241.cpp
@@ -1,7 +1,42 @@
 #include <gtest/gtest.h>
 
-int* diffWaysToCompute(char * input, int* returnSize){
+#define MAX_SIZE 10000
 
+int* dfs(char* input, int inputSize, int* returnSize)
+{
+    int* res = (int*)malloc(sizeof(int) * MAX_SIZE);
+    *returnSize = 0;
+    for (int i = 0; i < inputSize; i++) {
+        if (input[i] == '+' || input[i] == '-' || input[i] == '*') {
+            int leftSize = 0;
+            int* left = dfs(input, i, &leftSize);
+            int rightSize = 0;
+            int* right = dfs(input + i + 1, inputSize - i - 1, &rightSize);
+            for (int l = 0; l < leftSize; l++) {
+                for (int r = 0; r < rightSize; r++) {
+                    if (input[i] == '+') {
+                        res[(*returnSize)++] = left[l] + right[r];
+                    } else if (input[i] == '-') {
+                        res[(*returnSize)++] = left[l] - right[r];
+                    } else if (input[i] == '*') {
+                        res[(*returnSize)++] = left[l] * right[r];
+                    }
+                }
+            }
+        }
+    }
+    if (*returnSize == 0) {
+        int tmp = 0;
+        for (int i = 0; i < inputSize; i++) {
+            tmp = tmp * 10 + (input[i] - '0');
+        }
+        res[(*returnSize)++] = tmp;
+    }
+    return res;
+}
+
+int* diffWaysToCompute(char * input, int* returnSize){
+    return dfs(input, strlen(input), returnSize);
 }
 
 int main(int argc, char **argv)
