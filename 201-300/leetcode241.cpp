b142a010b84b19e3b7e69ed5fccce4bc0d81e641
zhouyuming <1006129869@qq.com>
2021-02-21 19:11:56 +0800

    update 241

---
+++ 201-300/leetcode241.cpp
@@ -0,0 +1,16 @@
+#include <gtest/gtest.h>
+
+int* diffWaysToCompute(char * input, int* returnSize){
+
+}
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
+TEST(LeetCode, num241x001)
+{
+
+}
