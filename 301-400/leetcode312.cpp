b45aa81ea89fd5c0c8e0426d7b54406390418ed1
zhouyuming <1006129869@qq.com>
2021-04-03 19:34:41 +0800

    update 312

--- 301-400/leetcode312.cpp
+++ 301-400/leetcode312.cpp
@@ -26,3 +26,24 @@ int maxCoins(int* nums, int numsSize)
     }
     return solve(0, numsSize + 1);
 }
+
+int maxCoins(int* nums, int numsSize)
+{
+    int rec[numsSize + 2][numsSize + 2];
+    memset(rec, 0, sizeof(rec));
+    int val[numsSize + 2];
+    val[0] = val[numsSize + 1] = 1;
+    for (int i = 1; i <= numsSize; i++) {
+        val[i] = nums[i - 1];
+    }
+    for (int i = numsSize - 1; i >= 0; i--) {
+        for (int j = i + 2; j <= numsSize + 1; j++) {
+            for (int k = i + 1; k < j; k++) {
+                int sum = val[i] * val[k] * val[j];
+                sum += rec[i][k] + rec[k][j];
+                rec[i][j] = fmax(rec[i][j], sum);
+            }        
+        }
+    }
+    return rec[0][numsSize + 1];
+}
